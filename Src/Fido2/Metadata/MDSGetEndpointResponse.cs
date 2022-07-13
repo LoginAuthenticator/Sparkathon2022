c39e1953c4cdd76d99d8b6cdb5172dfeb0144dd9
Bhushan Bapat <bhushan.bapat@nice.com>
2022-07-13 11:50:54 +0530

    first commit

---
+++ Src/Fido2/Metadata/MDSGetEndpointResponse.cs
@@ -0,0 +1,15 @@
+﻿#nullable disable
+
+using System.Text.Json.Serialization;
+
+namespace Fido2NetLib
+{
+    public sealed class MDSGetEndpointResponse
+    {
+        [JsonPropertyName("status")]
+        public string Status { get; set; }
+
+        [JsonPropertyName("result")]
+        public string[] Result { get; set; }
+    }
+}
